98eddea4ba21abc02b0a7d860bd1733a32e60309
Mohamed Saleh <mohxbox36021@gmail.com>
2024-10-20 16:02:48 +0300

    added vip-lounge

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000";
 
-// MIDDLEWARE
+// Middleware
 
 // Middleware to parse URL-encoded data from forms
 app.use(express.urlencoded({ extended: false }));
@@ -36,9 +36,20 @@ app.listen(port, () => {
     console.log(`The express app is ready on port ${port}!`);
 });
 
-// ROUTES
+// Public Routes
 
 // GET /
 app.get("/", async (req, res) => {
     res.render("index.ejs", { user: req.session.user });
+});
+
+// Protected Routes
+
+app.get("/vip-lounge", (req, res) => {
+    if (req.session.user) {
+        res.send(`Welcome to the party ${req.session.user.username}.`);
+    } else {
+        res.sendStatus(404);
+        // res.send("Sorry, no guests allowed.");
+    }
 });
