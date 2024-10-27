e8a31d36145f16aa635c76529953b9a9ba4626f9
theMohSaleh <120775304+theMohSaleh@users.noreply.github.com>
2024-10-27 12:18:03 +0300

    Added missing _id in auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -38,6 +38,7 @@ router.post('/sign-up', async (req, res) => {
     // respond back to the browser with created user signed in
     req.session.user = {
         username: newUser.username,
+        id_: newUser._id,
     }
 
     // send user back to home page
@@ -74,6 +75,7 @@ router.post('/sign-in', async (req, res) => {
     // create a session cookie
     req.session.user = {
         username: user.username,
+        id_: user._id,
     }
 
     // send user back to home page
